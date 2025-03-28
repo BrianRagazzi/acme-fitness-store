670ad090bf008ccad6a6f66155bf185e18f835f5
Corby <pcorby@vmware.com>
2025-03-28 10:56:13 -0500

    Upgraded to Spring AI 1.0.0-M6
    Added support for binding to MCP Servers

--- apps/acme-assist/src/main/java/com/example/acme/assist/ChatService.java
+++ apps/acme-assist/src/main/java/com/example/acme/assist/ChatService.java
@@ -2,7 +2,11 @@ package com.example.acme.assist;
 
 import com.example.acme.assist.model.AcmeChatMessage;
 import com.example.acme.assist.model.Product;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.micrometer.common.util.StringUtils;
+import io.modelcontextprotocol.client.McpClient;
+import io.modelcontextprotocol.client.McpSyncClient;
+import io.modelcontextprotocol.client.transport.HttpClientSseClientTransport;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.Message;
@@ -12,6 +16,8 @@ import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.ai.document.Document;
+import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,10 +25,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
+import javax.net.ssl.SSLContext;
+import java.net.http.HttpClient;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class ChatService {
@@ -35,6 +45,12 @@ public class ChatService {
 
     @Autowired
     private ChatClient chatClient;
+
+    @Autowired
+    private List<String> mcpServiceURLs;
+
+    @Autowired
+    private SSLContext sslContext;
 
     @Value("classpath:/prompts/chatWithoutProductId.st")
     private Resource chatWithoutProductIdResource;
@@ -64,12 +80,14 @@ public class ChatService {
 
     private List<String> chatWithProductId(Product product, List<AcmeChatMessage> chatRequestMessages) {
         // We have a specific Product
-        String question = chatRequestMessages.get(chatRequestMessages.size() - 1).getContent();
+        String question = chatRequestMessages.getLast().getContent();
 
         // step 1. Query for documents that are related to the question from the vector store
-        SearchRequest request = SearchRequest.query(question).
-                withTopK(5).
-                withSimilarityThreshold(0.4);
+        SearchRequest request = new SearchRequest.Builder().
+                query(question).
+                topK(5).
+                similarityThreshold(0.4).
+                build();
         List<Document> candidateDocuments = this.store.similaritySearch(request);
 
         // step 2. Create a SystemMessage that contains the product information in addition to related documents.
@@ -90,12 +108,14 @@ public class ChatService {
      */
     protected List<String> chatWithoutProductId(List<AcmeChatMessage> acmeChatRequestMessages) {
 
-        String question = acmeChatRequestMessages.get(acmeChatRequestMessages.size() - 1).getContent();
+        String question = acmeChatRequestMessages.getLast().getContent();
 
         // step 1. Query for documents that are related to the question from the vector store
-        SearchRequest request = SearchRequest.query(question).
-                withTopK(5).
-                withSimilarityThreshold(0.4);
+        SearchRequest request = SearchRequest.builder().
+                query(question).
+                topK(5).
+                similarityThreshold(0.4).
+                build();
         List<Document> relatedDocuments = store.similaritySearch(request);
 
 
@@ -103,7 +123,7 @@ public class ChatService {
         List<Message> messages = new ArrayList<>();
         SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(this.chatWithoutProductIdResource);
         String relatedDocsAsString = relatedDocuments.stream()
-                .map(entry -> String.format("Product Name: %s\nText: %s\n", entry.getMetadata().get("name"), entry.getContent()))
+                .map(entry -> String.format("Product Name: %s\nText: %s\n", entry.getMetadata().get("name"), entry.getText()))
                 .collect(Collectors.joining("\n"));
         Message systemMessage = systemPromptTemplate.createMessage(Map.of("context", relatedDocsAsString));
         messages.add(systemMessage);
@@ -115,23 +135,42 @@ public class ChatService {
     private List<String> addUserMessagesAndSendToAI(List<AcmeChatMessage> acmeChatRequestMessages, List<Message> messages) {
         // Convert to acme messages types to Spring AI message types
         for (AcmeChatMessage acmeChatRequestMessage : acmeChatRequestMessages) {
-            String role = acmeChatRequestMessage.getRole().toString().toUpperCase();
             messages.add(new UserMessage(acmeChatRequestMessage.getContent()));
         }
 
         // Call to OpenAI chat API
         Prompt prompt = new Prompt(messages);
-        ChatResponse aiResponse = this.chatClient.prompt(prompt).call().chatResponse();
 
-        // Process the result and return to client
-        List<String> response = processResult(aiResponse);
+        HttpClient.Builder clientBuilder = HttpClient.newBuilder()
+                .sslContext(sslContext)
+                .connectTimeout(Duration.ofSeconds(30));
+        try ( // Open all McpSyncClients in try-with-resources
+              Stream<McpSyncClient> mcpSyncClients = mcpServiceURLs.stream()
+                      .map(url -> {
+                          HttpClientSseClientTransport transport = new HttpClientSseClientTransport(
+                                  clientBuilder, url, new ObjectMapper());
+                          return McpClient.sync(transport).requestTimeout(Duration.ofSeconds(30)).build();
+                      })
+                      .peek(McpSyncClient::initialize)) {
+            ToolCallbackProvider[] toolCallbackProviders = mcpSyncClients
+                    .map(SyncMcpToolCallbackProvider::new)
+                    .toArray(ToolCallbackProvider[]::new);
 
-        return response;
+            ChatResponse aiResponse = this.chatClient.
+                    prompt(prompt).
+                    tools(toolCallbackProviders).
+                    call().
+                    chatResponse();
+
+            // Process the result and return to client
+            List<String> response = processResult(aiResponse);
+            return response;
+        }
     }
 
     public Message getProductDetailMessage(Product product, List<Document> documents) {
         String additionalContext = documents.stream()
-                .map(entry -> String.format("Product Name: %s\nText: %s\n", entry.getMetadata().get("name"), entry.getContent()))
+                .map(entry -> String.format("Product Name: %s\nText: %s\n", entry.getMetadata().get("name"), entry.getText()))
                 .collect(Collectors.joining("\n"));
         Map<String,Object> map = Map.of(
                 "name", product.getName(),
@@ -140,12 +179,12 @@ public class ChatService {
                 "fullDescription", product.getDescription(),
                 "additionalContext", additionalContext);
         SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(this.chatWithProductIdResource);
-        return systemPromptTemplate.create(map).getInstructions().get(0);
+        return systemPromptTemplate.create(map).getInstructions().getFirst();
     }
 
     private List<String> processResult(ChatResponse aiResponse) {
         List<String> response = aiResponse.getResults().stream()
-                .map(result -> result.getOutput().getContent())
+                .map(result -> result.getOutput().getText())
                 .filter(text -> !StringUtils.isEmpty(text))
                 .map(this::filterMessage)
                 .collect(Collectors.toList());
@@ -157,7 +196,7 @@ public class ChatService {
             throw new IllegalArgumentException("message shouldn't be empty.");
         }
 
-        if (acmeChatMessages.get(0).getRole() != MessageType.USER) {
+        if (acmeChatMessages.getFirst().getRole() != MessageType.USER) {
             throw new IllegalArgumentException("The first message should be in user role.");
         }
 
